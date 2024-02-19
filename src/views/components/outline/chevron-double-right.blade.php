4afcb9004e0207d527880c526c2eaa3da770c089
PH7-Jack <pedrolivertwd@gmail.com>
2024-02-19 03:30:25 -0300

    generate icons

--- src/views/components/outline/chevron-double-right.blade.php
+++ src/views/components/outline/chevron-double-right.blade.php
@@ -1,1 +1,1 @@
-<svg {{ $attributes }} stroke="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M11.25 4.5L18.75 12L11.25 19.5M5.25 4.5L12.75 12L5.25 19.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
+<svg {{ $attributes }} stroke="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M5.25 4.5L12.75 12L5.25 19.5M11.25 4.5L18.75 12L11.25 19.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
--- src/views/components/outline/minus.blade.php
+++ src/views/components/outline/minus.blade.php
@@ -1,1 +1,1 @@
-<svg {{ $attributes }} stroke="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M19.5 12L4.5 12" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
+<svg {{ $attributes }} stroke="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M5 12H19" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
